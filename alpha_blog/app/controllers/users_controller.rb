a35f4f752e077889a2462cf5c196b9f7b0d2c0a4
jerrychen <jerrychen040@gmail.com>
2016-09-02 09:42:19 +0800

    Add edit, update functionality and partial

--- alpha_blog/app/controllers/users_controller.rb
+++ alpha_blog/app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     #debugger
     @user= User.new(user_params)
@@ -15,9 +16,29 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+    @user = User.find(params[:id])
+
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params) #if update ok
+      #show the flash message at the top, success = green color
+      flash[:success] = "Your account was update successfully"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
+
+
 
   private
   def user_params
+    #get from html input by user.to save into params
+    #so def create, def update can get the username, email and password
     params.require(:user).permit(:username, :email, :password)
   end
 end
