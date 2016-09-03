549a71d060a104a504706a36ac2bea9b60338ddc
jerrychen <jerrychen040@gmail.com>
2016-09-03 15:27:48 +0800

    add login logout and sessions

--- alpha_blog/config/routes.rb
+++ alpha_blog/config/routes.rb
@@ -10,4 +10,11 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
 
   #resources :users
+
+
+  #for login route
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  #for logout route
+  delete 'logout', to: 'sessions#destroy'
 end
