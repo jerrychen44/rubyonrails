3f23dd0b880639f25c312a35920ca2f655685c34
jerrychen <jerrychen040@gmail.com>
2016-09-07 13:33:17 +0800

    Add article and category many to mnay association

--- alpha_blog/app/models/article.rb
+++ alpha_blog/app/models/article.rb
@@ -3,6 +3,11 @@ class Article < ActiveRecord::Base
   #for the view of article, it can belongs to many users.
   belongs_to :user
 
+  #for add the many to many to article_categories table ++
+  has_many :article_categories
+  has_many :categories, through: :article_categories
+  #--
+
   validates :title, presence: true, length:{minimum:3, maximum: 50}
   validates :description, presence: true, length:{minimum:10, maximum: 300}
 end
--- alpha_blog/app/models/category.rb
+++ alpha_blog/app/models/category.rb
@@ -1,5 +1,12 @@
 class Category < ActiveRecord::Base
 
+
+  #for add the many to many to article_categories table ++
+  has_many :article_categories
+  has_many :articles, through: :article_categories
+  #--
+
+
   #validates :name, presence: true
   #http://guides.rubyonrails.org/active_record_validations.html#presence
   #*** presence check 1.nil 2.blank string
